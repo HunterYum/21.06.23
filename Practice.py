ab538e0f9751e7afe6565658a81bc742cf516e59
HunterYum <chamch134@naver.com>
2021-06-23 10:14:12 +0900

    전달값, 반환값

--- Practice.py
+++ Practice.py
@@ -1,12 +1,30 @@
-#Chapter6 Quize
-from random import *
-cnt = 0
-for i in range(1, 51):
-    time = randrange(5, 51)
-    if 5 <= time <= 15:
-        print("[O] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
-        cnt += 1
-    else :
-        print("[x] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
-    
-print("총 탑승 승객 : {0}분".format(cnt))
+#함수 정의 후 호출
+def open_account():
+    print("새로운 계좌 생성됨")
+open_account()
+
+#입금
+def deposit(balance, money):
+    print("입금 완료. 잔액 : {0}".format(balance + money))
+    return balance + money
+
+balance = 0
+balance = deposit(balance, 1000)
+print(balance)
+
+def withdraw(balance, money):
+    if balance >= money:
+        print("출금 완료. 잔액 : {0}".format(balance - money))
+        return balance - money
+    else:
+        print("출금 불가능. 잔액 : {0}". format(balance))
+        return balance
+
+balance = withdraw(balance, 500)
+
+def withdraw_night(balance, money):
+    commission = 100
+    return commission, balance - money - commission
+
+comminssion, balance = withdraw_night(balance, 500)
+print("수수료 {0}원, 잔액 : {1}".format(comminssion, balance))
