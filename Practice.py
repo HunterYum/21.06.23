524da7fd5398c9a62d36e0c961ac3e0feec634e8
HunterYum <chamch134@naver.com>
2021-06-23 09:28:37 +0900

    반복문 1-2

--- Practice.py
+++ Practice.py
@@ -1,9 +1,22 @@
-#대기번호
+#커피 대기
+customer = "A"
+index = 5
+while index >=1:
+    print("{0}, 커피가 준비 됐습니다. {1}번 남았습니다.".format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피 보류")
 
-#방법1
-for waiting_num in [0, 1, 2, 3, 4]:
-     print("대기번호 {0}".format(waiting_num))
+#무한반복
+# index = 1
+# while True:
+#      print("{0}, 커피가 준비 됐습니다. {1}번 남았습니다.".format(customer, index))
+#      index += 1
 
-#방법2
-for waiting_num in range(1, 6): #1~5
-    print("대기번호 {0}".format(waiting_num))
+#whlie문 탈출예제
+customer = "A"
+person = "unknown"
+
+while person != customer :
+     print("{0}, 커피가 준비 됐습니다.".format(customer))
+     person = input("이름이")
