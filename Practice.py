084d8b7fc31ae958006870414881f6d1427dda26
HunterYum <chamch134@naver.com>
2021-06-23 13:27:09 +0900

    pickle모듈 사용하기

--- Practice.py
+++ Practice.py
@@ -1,23 +1,11 @@
-#파일 만들기
-# score_file = open("score.tex", "w", encoding="utf8")
-# print("math : 0", file=score_file)
-# print("en : 50", file=score_file)
-# score_file.close()
+import pickle
+# profile_file = open("profile.pickle", "wb")
+# profile = {"이름" : "A", "나이" : 20, "취미" : ["축구", "농구", "코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) #프로필에 있는 정보를 파일에 저장
+# profile_file.close()
 
-# score_file = open("score.tex", "a", encoding="utf8")
-# score_file.write("sci : 88")
-# score_file.write("\nco : 100")
-
-# #파일 읽기
-# score_file = open("score.tex", "r", encoding="utf8")
-# print(score_file.read())
-# score_file.close()
-
-#파일 글 하나씩 읽기
-score_file = open("score.tex", "r", encoding="utf8")
-print(score_file.readline(), end="")
-print(score_file.readline(), end="")
-print(score_file.readline(), end="")
-print(score_file.readline(), end="")
-score_file.close()
-
+profile_file = open("profile.pickle", 'rb')
+profile = pickle.load(profile_file)
+print(profile)
+profile_file.close()
