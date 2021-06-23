9bc117a42ccaebff4b27df261a943b7904aaf0ca
HunterYum <chamch134@naver.com>
2021-06-23 10:30:07 +0900

    default값

--- Practice.py
+++ Practice.py
@@ -1,30 +1,15 @@
-#함수 정의 후 호출
-def open_account():
-    print("새로운 계좌 생성됨")
-open_account()
+# def profile(name, age, main_lang):
+#     print("이름 : {0}\t주 사용 언어 : {2}"\
+#         .format(name, age, main_lang))
 
-#입금
-def deposit(balance, money):
-    print("입금 완료. 잔액 : {0}".format(balance + money))
-    return balance + money
+# profile("A", 20, "파이썬")
+# profile("B", 23, "자바")
 
-balance = 0
-balance = deposit(balance, 1000)
-print(balance)
+#기본값 사용하기
 
-def withdraw(balance, money):
-    if balance >= money:
-        print("출금 완료. 잔액 : {0}".format(balance - money))
-        return balance - money
-    else:
-        print("출금 불가능. 잔액 : {0}". format(balance))
-        return balance
+def profile(name, age = 17, main_lang = "파이썬"):
+    print("이름 : {0}\t주 사용 언어 : {2}"\
+        .format(name, age, main_lang))
 
-balance = withdraw(balance, 500)
-
-def withdraw_night(balance, money):
-    commission = 100
-    return commission, balance - money - commission
-
-comminssion, balance = withdraw_night(balance, 500)
-print("수수료 {0}원, 잔액 : {1}".format(comminssion, balance))
+profile("A")
+profile("B")
