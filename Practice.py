b0bfaeee33a3665049ec8616f0dfcf389b782e60
HunterYum <chamch134@naver.com>
2021-06-23 10:02:16 +0900

    chapter6 Quize

--- Practice.py
+++ Practice.py
@@ -1,14 +1,12 @@
-#숫자값에 더하기
-student = [1,2,3,4,5]
-print(student)
-student = [i+100 for i in student]
-print(student)
-
-#문자를 길이로 변환
-students = ["Apple", "Banana", "Cross"]
-students = [len(i) for i in students]
-print(students)
-
-#문자를 대문자로 변환
-students = [i.upper() for i in students]
-print(students)
+#Chapter6 Quize
+from random import *
+cnt = 0
+for i in range(1, 51):
+    time = randrange(5, 51)
+    if 5 <= time <= 15:
+        print("[O] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+        cnt += 1
+    else :
+        print("[x] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+    
+print("총 탑승 승객 : {0}분".format(cnt))
