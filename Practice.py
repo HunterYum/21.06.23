f8c38ec4b430b833d580a1c5f99bd5d24334de0a
HunterYum <chamch134@naver.com>
2021-06-23 13:38:41 +0900

    chapter8 quize

--- Practice.py
+++ Practice.py
@@ -1,10 +1,8 @@
-# import pickle
+#나도코딩 chapter8 Quize
 
-# with open("profile.pickle", "rb") as profile_file:
-#     print(pickle.load(profile_file))
-
-# with open("study.txt", "w", encoding="utf8") as study_file:
-#     study_file.write("파이썬을 열심히 공부중")
-
-with open("study.txt", "r", encoding="utf8") as study_file:
-    print(study_file.read())
+for i in range(1, 51):
+    with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file:
+        report_file.write("- {0} 주차 주간보고 -".format(i))
+        report_file.write("\n부서 :")
+        report_file.write("\n이름 :")
+        report_file.write("\n업무 요약 :")
