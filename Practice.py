3fbbedf444ba9740e178dbb3ceb9f472b3b5912e
HunterYum <chamch134@naver.com>
2021-06-23 13:32:15 +0900

    with함수

--- Practice.py
+++ Practice.py
@@ -1,11 +1,10 @@
-import pickle
-# profile_file = open("profile.pickle", "wb")
-# profile = {"이름" : "A", "나이" : 20, "취미" : ["축구", "농구", "코딩"]}
-# print(profile)
-# pickle.dump(profile, profile_file) #프로필에 있는 정보를 파일에 저장
-# profile_file.close()
+# import pickle
 
-profile_file = open("profile.pickle", 'rb')
-profile = pickle.load(profile_file)
-print(profile)
-profile_file.close()
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
+
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부중")
+
+with open("study.txt", "r", encoding="utf8") as study_file:
+    print(study_file.read())
