5fa8746f95b14132b6c047f48d91773a09c9a5f3
HunterYum <chamch134@naver.com>
2021-06-24 13:46:57 +0900

    exception handling2

--- Practice.py
+++ Practice.py
@@ -1,14 +1,23 @@
+class BigNumberError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
 try:
-    print("나누기 전용 계산기")
-    ns = []
-    ns.append(int(input("첫 번째 숫자 : ")))
-    ns.append(int(input("두 번째 숫자 : ")))
-    ns.append(int(ns[0]/ns[1]))
-    print("{0}/{1} = {2}".format(ns[0], ns[1], ns[1]/ns[2]))
+    print("한 자리 숫자 나누기 전용 계산기")
+    n1 = int(input("첫번째 숫자 : "))
+    n2 = int(input("두번째 숫자 : "))
+    if n1 >= 10 or n2 >= 10:
+        raise BigNumberError("입력값 : {0}, {1}".format(n1, n2))
+    print("{0}/{1} = {2}".format(n1, n2, int(n1/n2)))
+
 except ValueError:
-    print("오류 발생")
-except ZeroDivisionError as err:
+    print("잘못된 값 입력. 한 자리 숫자만 입력 바람.")
+except BigNumberError as err:
+    print("에러 발생. 한 자리 숫자만 입력")
     print(err)
-except Exception as err:
-    print("알수없음")
-    print(err)
+
+finally:
+    print("계산기 종료")
