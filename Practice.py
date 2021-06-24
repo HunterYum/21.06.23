efc5c876acfc81d3c610c6250435498870bf65d0
HunterYum <chamch134@naver.com>
2021-06-24 13:54:26 +0900

    chapter10 Quize

--- Practice.py
+++ Practice.py
@@ -1,23 +1,27 @@
-class BigNumberError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
+#나도코딩 chapter10 Quize
+class SoldOutError(Exception):
+    pass
 
-    def __str__(self):
-        return self.msg
+chicken = 10
+waiting = 1
 
-try:
-    print("한 자리 숫자 나누기 전용 계산기")
-    n1 = int(input("첫번째 숫자 : "))
-    n2 = int(input("두번째 숫자 : "))
-    if n1 >= 10 or n2 >= 10:
-        raise BigNumberError("입력값 : {0}, {1}".format(n1, n2))
-    print("{0}/{1} = {2}".format(n1, n2, int(n1/n2)))
+while(True):
+    try:
+        print("[남은 치킨 : {0}]".format(chicken))
+        order = int(input("치킨 몇 마리 주문하시겠습니까?"))
+        if order > chicken:
+            print("재료가 부족합니다.")    
+        elif order <= 0:
+            raise ValueError    
+        else:
+            print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다.".format(waiting, order))
+            waiting += 1
+            chicken -= order
 
-except ValueError:
-    print("잘못된 값 입력. 한 자리 숫자만 입력 바람.")
-except BigNumberError as err:
-    print("에러 발생. 한 자리 숫자만 입력")
-    print(err)
-
-finally:
-    print("계산기 종료")
+        if chicken == 0:
+            raise SoldOutError
+    except ValueError:
+        print("잘못된 값을 입력하였습니다.")
+    except SoldOutError:
+        print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+        break
