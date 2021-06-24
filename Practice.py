27278f41685b53b464275a7dc64559deff23e000
HunterYum <chamch134@naver.com>
2021-06-24 10:02:14 +0900

    chapter9 quize

--- Practice.py
+++ Practice.py
@@ -1,163 +1,27 @@
-from random import *
+#나도코딩 chapter9 Quize
+class House:
+    # 매물 초기화 : 위치, 건물 종류, 매물 종류, 가격, 준공년도
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.completion_year = completion_year
 
-# 일반 유닛
-class Unit:
-    def __init__(self, name, hp, speed):
-        self.name = name
-        self.hp = hp
-        self.speed = speed
-        print("{0} 유닛이 생성되었습니다.".format(name))
+    # 매물 정보 표시
+    def show_detail(self):
+        print(self.location, self.house_type, self.deal_type, 
+        self.price, self.completion_year)
 
-    def move(self, location):
-        print("[지상 유닛 이동]")
-        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
-            .format(self.name, location, self.speed))
+houses = []
+house1 = House("강남", "아파트", "매매", "10억", "2010년")
+house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+house3 = House("송파", "빌라", "월세", "500/50", "2000년")
 
-    def damaged(self, damage):
-        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
-        self.hp -= damage
-        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
-        if self.hp <= 0:
-            print("{0} : 파괴되었습니다.".format(self.name))
+houses.append(house1)
+houses.append(house2)
+houses.append(house3)
 
-
-
-# 공격 유닛
-class AttackUnit(Unit):
-    def __init__(self, name, hp, speed, damage):
-        Unit.__init__(self, name, hp, speed)
-        self.damage = damage
-
-    def attack(self, location):
-        print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]" \
-            .format(self.name, location, self.damage))
-
-#마린
-class Marine(AttackUnit):
-    def __init__(self):
-        AttackUnit.__init__(self, "마린", 40, 1, 5)
-
-    #스팀팩 
-    def stimpack(self):
-        if self.hp > 10:
-            self.hp -= 10
-            print("{0} : 스팀팩을 사용합니다. (HP 10 감소)".format(self.name))
-        else:
-            print("{0} : 체력이 부족하여 스팀팩을 사용하지 않습니다.".format(self.name))
-
-#탱크
-class Tank(AttackUnit):
-    #시즈모드
-    seize_developed = False
-
-    def __init__(self):
-        AttackUnit.__init__(self, "탱크", 150, 1, 35)
-        self.seize_mode = False
-
-    def set_seize_mode(self):
-        if Tank.seize_developed == False:
-            return
-
-        if self.seize_mode == False:
-            print("{0} : 시즈모드로 전환합니다.".format(self.name))
-            self.damage *=2
-            self.seize_mode = True
-
-        else:
-            print("{0} : 시즈모드로 해제합니다.".format(self.name))
-            self.damage /=2
-            self.seize_mode = False
-
-
-# 드랍쉽 : 공중 유닛, 수송기, 공격 불가
-
-# 날 수 있는 기능을 가진 클래스
-class Flyable:
-    def __init__(self, flying_speed):
-        self.flying_speed = flying_speed
-
-    def fly(self, name, location):
-        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
-            .format(name, location, self.flying_speed))
-
-# 공중 공격 유닛
-class FlyableAttackUnit(AttackUnit, Flyable):
-    def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, 0, damage)
-        Flyable.__init__(self, flying_speed)
-
-    def move(self, location):
-        print("[공중 유닛 이동]")
-        self.fly(self.name, location)
-
-class Wraith(FlyableAttackUnit):
-    def __init__(self):
-        FlyableAttackUnit.__init__(self, "레이스", 80, 20, 5)
-        self.clocked = False
-        
-    def clocking(self):
-        if self.clocked == True:
-            print("{0} : 클로킹 모드 해제합니다.".format(self.name))
-            self.clocked = False
-        else:
-            print("{0} : 클로킹 모드 설정합니다.".format(self.name))
-            self.clocked = True
-
-
-# 벌쳐 : 지상 유닛, 기동성이 좋음
-vulture = AttackUnit("벌쳐", 80, 10, 20) # 지상 speed 10
-
-# 배틀크루저 : 공중 유닛, 체력도 굉장히 좋음, 공격력도 좋음
-battlecruiser = FlyableAttackUnit("배틀크루저", 500, 25, 3)
-
-vulture.move("11시")
-# battlecruiser.fly(battlecruiser.name, "9시")
-battlecruiser.move("9시") # 오버라이딩된 move() 호출
-
-def game_start():
-    print("[알림] 새로운 게임을 시작합니다.")
-
-def game_over():
-    print("player : GG")
-    print("[Player] 님이 퇴장하셨습니다.")
-
-game_start()
-
-m1 = Marine()
-m2 = Marine()
-m3 = Marine()
-
-t1 = Tank()
-t2 = Tank()
-
-w1 = Wraith()
-
-attack_units = []
-attack_units.append(m1)
-attack_units.append(m2)
-attack_units.append(m3)
-attack_units.append(t1)
-attack_units.append(t2)
-attack_units.append(w1)
-
-for unit in attack_units:
-    unit.move("1시")
-
-Tank.seize_developed = True
-print("[알림] 탱크 시즈 모드 개발이 완료되었습니다.")
-
-for unit in attack_units:
-    if isinstance(unit, Marine):
-        unit.stimpack()
-    elif isinstance(unit, Tank):
-        unit.set_seize_mode()
-    elif isinstance(unit, Wraith):
-        unit.clocking()
-
-for unit in attack_units:
-    unit.attack("1시")
-
-for unit in attack_units:
-    unit.damaged(randint(5, 21))
-
-game_over()
+print("총 {0}대의 매물이 있습니다.".format(len(houses)))
+for house in houses:
+    house.show_detail()
